ada4c6095ba868ee8b7d4f2c5f077a6bc2736516
supernoided <55270738+supernoided@users.noreply.github.com>
2019-11-25 23:43:41 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(".owl-carousel").owlCarousel({
+  loop:true,
+  dots: true,
+  autoWidth: true,
+  center: true,
+  nav: true,
+  responsiveClass:true,
+  responsive:{
+        0:{
+            items:1,
+            margin:0,
+            
+        },
+       601:{
+            items:1,
+            margin:100,
+            
+        },
+        901:{
+            items: 1.3,
+            margin:358,
+        }
+  }
+});    
